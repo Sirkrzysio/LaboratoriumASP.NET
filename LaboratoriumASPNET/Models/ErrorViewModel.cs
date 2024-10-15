f67b3d9838403bbcc14ddce731612814c4926eec
krzysztof.rychter <krzysztof.rychter@microsoft.wsei.edu.pl>
2024-10-15 17:05:20 +0200

    init

---
+++ LaboratoriumASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LaboratoriumASPNET.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
