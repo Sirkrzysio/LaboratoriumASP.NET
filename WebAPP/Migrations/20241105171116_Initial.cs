8d04e7a151a9dfccdbc5282606ce3c44e75b8658
krzysztof.rychter <krzysztof.rychter@microsoft.wsei.edu.pl>
2024-11-05 18:12:38 +0100

    poprawka

---
+++ WebAPP/Migrations/20241105171116_Initial.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebAPP.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    birth_date = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Category = table.Column<int>(type: "INTEGER", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contacts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "contacts",
+                columns: new[] { "Id", "birth_date", "Category", "Created", "Email", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { 1, new DateOnly(1990, 1, 1), 0, new DateTime(2024, 11, 5, 18, 11, 15, 229, DateTimeKind.Local).AddTicks(1059), "", "Jan", "Kowalski", "123 456 789" },
+                    { 2, new DateOnly(1995, 5, 5), 0, new DateTime(2024, 11, 5, 18, 11, 15, 229, DateTimeKind.Local).AddTicks(1104), "", "Anna", "Nowak", "987 654 321" },
+                    { 3, new DateOnly(2000, 10, 10), 0, new DateTime(2024, 11, 5, 18, 11, 15, 229, DateTimeKind.Local).AddTicks(1167), "", "Piotr", "Wiśniewski", "456 789 123" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contacts");
+        }
+    }
+}
